6b094a8e01489d1951ba809b242064caec0bfc46
s24013161 <s24013161@ousl.lk>
2024-12-26 21:53:54 +0530

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,12 +1,2 @@
 print("Hello, GitHub!")
-import requests
 
-# Replace 'your_username' and 'your_repository' with your GitHub details
-url = "https://raw.githubusercontent.com/your_username/your_repository/main/hello.py"
-response = requests.get(url)
-
-if response.status_code == 200:
-    print("Fetched script content:")
-    print(response.text)
-else:
-    print(f"Failed to fetch script. Status code: {response.status_code}")
